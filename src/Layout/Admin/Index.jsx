d8f1844946810f415791c643f244b8c440eb8f89
yusefbahrami <yusef.b1381@gmail.com>
2023-09-09 23:53:16 +0330

    102 - something

--- src/Layout/Admin/Index.jsx
+++ src/Layout/Admin/Index.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "./navbar/index";
 import Sidebar from "./sidebar/index";
 import AdminContextContainer from "../../Context/AdminLayoutContext";
 import Content from "../../Pages/Content";
-import axios from "axios";
 import { Navigate } from "react-router-dom";
 import { useIsLogin } from "../../Hooks/authHook";
 
